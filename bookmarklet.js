ec2e542a81a54bf59b367e90b54ff5abb03335b4
junglebaboon45 <96539564+owen-tc@users.noreply.github.com>
2022-03-23 12:37:55 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function () {let code = document.createElement('script');code.src = 'https://crownjs.anirudhiscool.repl.co/script.js';document.body.appendChild(code);}())
+javascript:(function () {let code = document.createElement('script');code.src = 'https://crownjjs.vercel.app/crown.js';document.body.appendChild(code);}())
